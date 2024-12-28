2501bab5be5200d54c4a3b90f6d9508ce7b6e12d
omwami <ianalpha@protonmail.com>
2024-12-28 22:40:54 +0300

    setup

---
+++ app/src/App.jsx
@@ -0,0 +1,15 @@
+import './assets/styles/App.css'
+import "./assets/styles/Landing.css";
+
+import LandingPage from './components/Landingpage'
+
+function App() {
+
+  return (
+    <>
+      <LandingPage />
+    </>
+  )
+}
+
+export default App
