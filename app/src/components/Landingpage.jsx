94b9b5cd93ce17abdc8c20d09621c2b4db0ef112
omwami <ianalpha@protonmail.com>
2025-02-23 16:02:14 +0300

    tinified png files

--- app/src/components/Landingpage.jsx
+++ app/src/components/Landingpage.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { faMapMarkerAlt } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import Slider from "react-slick";
+import LazyLoad from "react-lazyload";
 
 import KidsDiaperImage from "../assets/images/Kids diapers collage design.png";
 import AdultDiaperImage from "../assets/images/Adult diapers collage design.png";
